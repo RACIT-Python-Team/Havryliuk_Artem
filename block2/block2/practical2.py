9de6823c1ff17a344db0e3d8756c3ccb32ca642c
Artemius1111 <artemgavryliuk577@gmail.com>
2025-12-06 21:54:39 +0200

    block2

--- block2/block2/practical2.py
+++ block2/block2/practical2.py
@@ -16,20 +16,19 @@ while True:
 
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = hands.process(frame_rgb)
-
-    c = 0
+    han = 0
     text = ""
     if results.multi_hand_landmarks:
-        c = len(results.multi_hand_landmarks)
+        han = len(results.multi_hand_landmarks)
         for hand in results.multi_hand_landmarks:
             mp_draw.draw_landmarks(frame, hand, mp_hands.HAND_CONNECTIONS, l, h)
-
+            
             h_img, w_img, _ = frame.shape
             x = int(sum([lm.x for lm in hand.landmark]) / 21 * w_img)
             y = int(sum([lm.y for lm in hand.landmark]) / 21 * h_img)
             text += f'({x},{y}) '
         cv2.putText(frame , f'Hands detected and coord: {len(results.multi_hand_landmarks) if results.multi_hand_landmarks else 0} {text}',
-                (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
+                (0, 450), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
     cv2.imshow('Hands Detection and coord', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
